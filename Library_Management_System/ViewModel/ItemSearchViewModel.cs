07d64f7544d6addac0d2af29de1d92a7a5a37a46
GrblackStar <hristiana_h@yahoo.com>
2023-05-18 22:06:25 +0300

    reading columns to list. display info not working

--- Library_Management_System/ViewModel/ItemSearchViewModel.cs
+++ Library_Management_System/ViewModel/ItemSearchViewModel.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using Library_Management_System.Model;
+using Library_Management_System.View;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
 
 namespace Library_Management_System.ViewModel
 {
@@ -12,8 +18,95 @@ namespace Library_Management_System.ViewModel
     {
         private readonly LibraryContext _dbContext;
         private ObservableCollection<TableName> _tableNames;
+        private string _selectedTable;
+        private List<string> _columnsFromDatabase;
 
-        private string _selectedItem;
+        private ObservableCollection<dynamic> _entities;
+        private ICollectionView view;
+
+        private ICommand searchItemCommand;
+
+
+
+
+
+
+        public ICommand SearchTableCommand
+        {
+            get
+            {
+                if (searchItemCommand == null)
+                {
+                    searchItemCommand = new RelayCommand(
+                            param => this.SearchTable(),
+                        param => this.CanCallMethod());
+
+                }
+                return searchItemCommand;
+            }
+        }
+
+        public ObservableCollection<TableName> TableNames
+        {
+            get { return _tableNames; }
+            set
+            {
+                _tableNames = value;
+                OnPropertyChanged("TableNames");
+            }
+        }
+
+        public string SelectedTable
+        {
+            get { return _selectedTable; }
+            set
+            {
+                if (_selectedTable != value)
+                {
+                    _selectedTable = value;
+                    OnPropertyChanged("SelectedTable");
+                }
+            }
+        }
+
+        public List<string> ColumnNamesFromDatabase
+        {
+            get { return _columnsFromDatabase; }
+            set
+            {
+                _columnsFromDatabase = value;
+                OnPropertyChanged("TableNames");
+            }
+        }
+
+        public ObservableCollection<dynamic> Entities
+        {
+            get { return _entities; }
+            set
+            {
+                if (_entities != value)
+                {
+                    _entities = value;
+                    OnPropertyChanged("Entities");
+                }
+            }
+        }
+
+        /*
+        private ListView ItemListView;
+        public ItemSearchViewModel()
+        {
+            // Other code...
+
+            Loaded += ItemSearchViewModel_Loaded;
+        }
+
+        private void ItemSearchViewModel_Loaded(object sender, RoutedEventArgs e)
+        {
+            ItemListView = FindName("ItemListView") as ListView;
+        }
+        */
+
 
         public ItemSearchViewModel()
         {
@@ -25,27 +118,69 @@ namespace Library_Management_System.ViewModel
             );
         }
 
-        public ObservableCollection<TableName> TableNames
+
+
+        // method to get the columns from the database table:
+        private List<string> GetTableColumns(string tableName)
         {
-            get => _tableNames;
-            set
-            {
-                _tableNames = value;
-                OnPropertyChanged("TableNames");
-            }
+            List<string> columns = _dbContext.Database.SqlQuery<string>(
+                $"SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '{tableName}'")
+                .ToList();
+
+            return columns;
         }
 
-        public string SelectedItem
+        private void SearchTable()
         {
-            get { return _selectedItem; }
-            set
+            List<string> columns = GetTableColumns(SelectedTable);
+            // make columns:
+
+
+            GetDataByTable(SelectedTable);
+
+        }
+
+        private void GetDataByTable(string selectedTable)
+        {
+            // static implementation:
+            if (selectedTable == "Books")
             {
-                if (_selectedItem != value)
+                using (var db = new LibraryContext())
                 {
-                    _selectedItem = value;
-                    OnPropertyChanged("SelectedItem");
+                    List<Book> books = db.Books.ToList();
+                    Entities = new ObservableCollection<dynamic>(books);
+
+                    view = CollectionViewSource.GetDefaultView(_entities);
                 }
+                
+                OnPropertyChanged("Entities");
             }
+
+            /*
+            using (var db = new LibraryContext())
+            {
+                List<dynamic> items = db.{selectedTable}.ToList();
+                Entities = new ObservableCollection<dynamic>(items);
+            }
+            */
+
+        }
+
+        private void InitializeListViewColumns(List<string> columns)
+        {
+            
+            var listViewColumns = new ObservableCollection<GridViewColumn>();
+            foreach (var columnName in columns)
+            {
+                var column = new GridViewColumn
+                {
+                    Header = columnName,
+                    DisplayMemberBinding = new Binding(columnName)
+                };
+                listViewColumns.Add(column);
+            }
+            // get the list view by name
+            //_listView.Columns = listViewColumns;
         }
 
 
@@ -55,6 +190,15 @@ namespace Library_Management_System.ViewModel
 
 
 
+
+
+
+
+
+        private bool CanCallMethod()
+        {
+            return true;
+        }
 
         public event PropertyChangedEventHandler PropertyChanged;
         protected virtual void OnPropertyChanged(string propertyName)
